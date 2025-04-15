09c3cdd6b5eb1babc60510a43a5b18814161d431
mi-pit <mi.pitner@gmail.com>
2025-04-15 14:35:28 +0200

    fixed transpiler.sh Convertor.py call path & Convertor.py file stream exception handling

--- transpiler.sh
+++ transpiler.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+if ! test -f "$1"; then
+  echo "File '$1' does not exist."
+fi
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 ORIGINAL_C_CODE_FILE="$1".c
 STRUCT_CODE_FILE="$1""--code.c"
 HEADER_FILE="$1"--header.h
 
-python3.10 "***REMOVED***MyC/Convertor.py" "$1" 2>/dev/null || exit 1
+if ! python3.10 "$SCRIPT_DIR/Convertor.py" "$1";
+then
+  exit 1
+fi
+
 clang-format -i "$ORIGINAL_C_CODE_FILE"
 clang-format -i "$STRUCT_CODE_FILE"
 clang-format -i "$HEADER_FILE"
