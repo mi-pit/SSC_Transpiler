9f7fc5f78ff0682b74aa193bcc4595d8481edbe4
mi-pit <mi.pitner@gmail.com>
2025-11-25 02:51:01 +0100

    Remove python stuff

--- text_manipulation.py
+++
@@ -1,4 +0,0 @@
-def get_text_separated(token):
-    if not hasattr(token, "getChildren") or not token.getChildren():
-        return token.getText()
-    return " ".join([get_text_separated(child) for child in token.getChildren()])
