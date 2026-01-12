6d80226c06d149f9ef2c40febb4ad7b5ee5b276b
mi-pit <mi.pitner@gmail.com>
2026-01-12 17:52:42 +0100

    Add nullability annotations

---
+++ SSC-C/src/main/java/cz/mipit/sscc/util/annotations/Nullable.java
@@ -0,0 +1,10 @@
+package cz.mipit.sscc.util.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Documented
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.TYPE_USE})
+public @interface Nullable {
+}
