fae8748e42c53e5cda73ce986a807c5076fc2358
mi-pit <mi.pitner@gmail.com>
2026-01-18 19:26:09 +0100

    Update annotations' docs

--- SSC-C/src/main/java/cz/mipit/sscc/util/annotations/NotNull.java
+++ SSC-C/src/main/java/cz/mipit/sscc/util/annotations/NotNull.java
@@ -4,6 +4,12 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
+/**
+ * Variable or method return value should not be null.
+ * <p>
+ * Assigning null values leads to undefined behavior.
+ * </p>
+ */
 @Documented
 @Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.TYPE_USE})
 public @interface NotNull {
--- SSC-C/src/main/java/cz/mipit/sscc/util/annotations/Nullable.java
+++ SSC-C/src/main/java/cz/mipit/sscc/util/annotations/Nullable.java
@@ -4,6 +4,12 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
+/**
+ * Variable or method return value may be null.
+ * <p>
+ * Assigning null values has well-defined behavior.
+ * </p>
+ */
 @Documented
 @Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.TYPE_USE})
 public @interface Nullable {
