de49a5100e0926630634bf0e48dcce9e93ac8fda
mi-pit <mi.pitner@gmail.com>
2026-02-18 05:41:34 +0100

    Remove more unused stuff

--- SSC-C/src/main/java/cz/mipit/sscc/ssc/exceptions/children/AntlrException.java
+++ SSC-C/src/main/java/cz/mipit/sscc/ssc/exceptions/children/AntlrException.java
@@ -6,12 +6,7 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Token;
 
 public class AntlrException extends SSCTranspilerException {
-
     public AntlrException(String message, Token token, CommonTokenStream tokens, InputFile inputFile) {
         super(Type.Antlr_parser, message, token, tokens, inputFile);
-    }
-
-    public AntlrException(Token tok, CommonTokenStream tokens, InputFile inputFile) {
-        this("Could not parse token '" + tok.getText() + "'", tok, tokens, inputFile);
     }
 }
--- SSC-C/src/main/java/cz/mipit/sscc/util/ExitValue.java
+++ SSC-C/src/main/java/cz/mipit/sscc/util/ExitValue.java
@@ -4,20 +4,9 @@ public enum ExitValue {
     SUCCESS /* = 0 */,
     INVALID_ARGUMENTS,
     TRANSPILATION_FAIL,
-    C_PREPROCESSING_FAIL,
-    C_VERIFICATION_FAIL,
     C_COMPILATION_FAIL,
-    IO_EXCEPTION,
     LIBRARY_NOT_FOUND,
     ;
-
-    public boolean isSuccess() {
-        return this == SUCCESS;
-    }
-
-    public boolean isFailure() {
-        return this != SUCCESS;
-    }
 
     @Override
     public String toString() {
