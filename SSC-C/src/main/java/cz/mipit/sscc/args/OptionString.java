784e6c2d8f1f8793cb1f6bdf02137a5afadd278e
mi-pit <mi.pitner@gmail.com>
2026-03-08 12:53:26 +0100

    Fix `OptionString`

--- SSC-C/src/main/java/cz/mipit/sscc/args/OptionString.java
+++ SSC-C/src/main/java/cz/mipit/sscc/args/OptionString.java
@@ -1,33 +1,31 @@
 package cz.mipit.sscc.args;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.Set;
 import java.util.StringJoiner;
 
 public class OptionString {
-    private final List<String> values;
+    private final Set<String> values;
 
     public OptionString(String... values) {
         if (values.length == 0) {
             throw new IllegalArgumentException("Must provide at least one value");
         }
-        this.values = new ArrayList<>(Arrays.asList(values));
+        // use `copyOf` to throw if an element is null
+        this.values = Set.copyOf(Arrays.asList(values));
     }
 
     public String formatted(final boolean hasArgument) {
+        final String argString = (hasArgument ? " ‹arg›" : "");
         final StringJoiner joiner = new StringJoiner(", ");
         for (final String value : values) {
-            joiner.add(value + (hasArgument ? " ‹arg›" : ""));
+            joiner.add(value + argString);
         }
 
         return joiner.toString();
     }
 
     public boolean matches(String value) {
-        Objects.requireNonNull(value);
-
         return values.contains(value);
     }
 }
